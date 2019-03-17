cff2ae42c362cdddf98e44ce8b366c312d5b0942
ShivamBandral <shivambandral@gmail.com>
2019-03-17 16:00:01 +0530

    Added 1-8 C++ basic programs

---
+++ 1.Basics/6. Reassigning Variable.cpp
@@ -0,0 +1,15 @@
+/*
+Reassigning values to a initailized variable.
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int tuna= 99;
+	tuna = 76;
+	
+	cout<<tuna;
+	return 0;
+	
+}
---
+++ 1.Basics/8. Relational Operators.cpp
@@ -0,0 +1,47 @@
+/*
+Relational operators in C++
+*/
+# include<bits/stdc++.h> // This is a header file which includes most of the header files removing the hassle of including them individually everytime you work with certain things.
+using namespace std;
+ 
+int main()
+{
+int a= 96;
+int b= 76;
+
+ 
+if(5>3) // greater than test
+{cout<<" bucky is awesome1 \n";
+}
+
+if(5>3) // lesser than test
+{cout<<" bucky is awesome2 \n";
+}
+
+if(3>3) // greater than equal to test
+{cout<<" bucky is awesome3 \n";
+}
+
+if(1<=3) // lesser than equal to test
+{cout<<" bucky is awesome4 \n";
+}
+
+
+// these were relational tests not we will be doing quality tests
+
+if(3==3) // equal to test
+{cout<<" bucky is awesome5 \n";
+}
+
+if(5!=3) // not equal to test
+{cout<<" bucky is awesome6 \n";
+}
+
+// we usually run tests on variables
+
+if(a > b)
+{cout<<" greater value is "<<a<< endl;
+}
+
+return 0;
+}
