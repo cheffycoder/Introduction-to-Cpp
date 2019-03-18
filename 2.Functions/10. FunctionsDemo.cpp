d3eb5c2590bef569a0f1f32935a38f4196c50b62
ShivamBandral <shivambandral@gmail.com>
2019-03-18 14:37:50 +0530

    Added Functions

---
+++ 2.Functions/10. FunctionsDemo.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printCrap(int x1)// 
+{cout<<"my favourite number is "<< x1 << endl; ;
+}
+int main(){
+	int x2;
+	cout<< "Enter my favourite number"<<endl;
+	cin >> x2;
+	printCrap(x2);
+	printCrap(20);
+	printCrap(854);
+	
+	return 0;
+}
