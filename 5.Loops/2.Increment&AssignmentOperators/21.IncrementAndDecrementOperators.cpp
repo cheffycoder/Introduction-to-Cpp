89e8d6b8bbd3adf209c1d6dc10493bf1e5d0aec9
ShivamBandral <shivambandral@gmail.com>
2019-03-22 20:37:43 +0530

    Added Increment and Assignment Operators

---
+++ 5.Loops/2.Increment&AssignmentOperators/21.IncrementAndDecrementOperators.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+/*	assignment operators
+	int x= 10;
+	x+=5;//x= x+5
+	x-=5;//x= x-5
+	x*=5;//x= x*5
+	x/=5;//x= x/5
+	x%=5;//x= x%5
+	cout<< x<< endl; 
+*/	
+
+
+//increment operators
+int x=20;
+//post increment i.e after assigning the value to variable, the value is incremented 
+cout<< x++ << endl;
+cout<< x<< endl;
+//pre increment
+cout<< ++x << endl;
+cout<< x<< endl;
+//post decrement
+cout<< x-- << endl;
+cout<< x<< endl;
+//pre decrement
+cout<< --x << endl;
+cout<< x<< endl;
+	
+	
+		
+	
+	return 0;
+}
