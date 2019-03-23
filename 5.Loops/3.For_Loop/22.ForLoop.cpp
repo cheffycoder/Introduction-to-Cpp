0f2a1bdd39232b4cae1114740902d248a66f9cd8
ShivamBandral <shivambandral@gmail.com>
2019-03-23 20:38:41 +0530

    Added For Loop introductory programs

---
+++ 5.Loops/3.For_Loop/22.ForLoop.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	//for(intialization; loop continuation condition; incrementation)
+	for(int i=1;i<10; i++){
+		cout<<i<< endl;
+	}
+	
+	for(int i=5;i<50; i+=5){
+		cout<<i<< endl;
+	}
+	return 0;
+}
