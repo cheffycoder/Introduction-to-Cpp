832771fe632720ab4014cb0bfbec7bf001e3b3cd
ShivamBandral <shivambandral@gmail.com>
2019-04-02 21:48:57 +0530

    Added Multi-Dimensional Array programs

---
+++ 7.Arrays/2.MultiDimensionalArray/36.cpp
@@ -0,0 +1,18 @@
+//multi dimensional array
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	
+	int sally[2][3] = {{2,3,4},{8,9,10}};
+	// sally [row] [columns]= each array is row and each value is a column i.e 
+	
+	//2,3,4 - array 1
+	//8,9,10 - array 2
+	
+	cout<< sally[0][2]<<endl;
+	cout<< sally[1][1]<<endl;
+	
+	return 0;
+}
