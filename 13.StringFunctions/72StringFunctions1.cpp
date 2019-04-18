97ae32d80bf24a24f146733feec7c250c47e7be4
ShivamBandral <shivambandral@gmail.com>
2019-04-18 21:02:08 +0530

    Added String Class and Functions

---
+++ 13.StringFunctions/72StringFunctions1.cpp
@@ -0,0 +1,45 @@
+// more string fn
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+	/*			SUBTRING FN()-  stringname.substr(begin, no of char you want to read)
+	string s1("omg i think i am preggy!!?");
+	cout<<s1.substr(17, 7)<<endl;
+	// this takes 2 parameters substr(begin index, how many characters you want to print)
+	//it prints begin and 7 characters including begin index
+	*/	
+	
+	
+	
+	/*			SWAP FN()- string1name.swap(string2name)
+	
+	string s2("apples");
+	string s3("beans");
+	cout << s2<< endl;
+	cout << s3<< endl;
+	
+	s2.swap(s3);// this fn takes 1 string parameter to tell it which string to swap it with 
+	cout << s2<< endl;
+	cout << s3<< endl;
+	*/
+	
+	
+	
+	/*			FIND FN()-  stringname.find("character or substring you want to find"); this will search for the substring from left to right
+				RFINDFN()-	stringname.rfind("character or substring you want to find");    this will search for the substring from right to left 
+				
+					
+	// its going to give us the index that where in the string it is
+	string s4("ham is spam oh yes i am!");
+	cout<< s4<< endl;
+	cout<< s4.find("am")<<endl;// this will give you the first occurence of the the substring or character
+	cout<< s4.rfind("am")<<endl;// this will give us the last occurence of the substring or character you are looking for
+	// this works right to left
+	
+	*/
+	return 0;
+}
